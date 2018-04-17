3fbaceb0d8e2fa94f2a4dc8fa29738e9e77f4d31
Katie Mikul <katie.heding@gmail.com>
2018-04-17 11:36:16 -0500

    create listening server

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express'); //links in express from the Nodes Modules
+
+const app = express();
+const PORT = 5000;
+
+app.listen(PORT);  //server needs to constantly be moving (request, receive, request, receive, etc.)
