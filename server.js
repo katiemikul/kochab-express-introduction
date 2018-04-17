1aa4fbfdfddebb11735408a6f4be47b311496442
Katie Mikul <katie.heding@gmail.com>
2018-04-17 11:52:45 -0500

    add text

--- server.js
+++ server.js
@@ -3,4 +3,16 @@ const express = require('express'); //links in express from the Nodes Modules
 const app = express();
 const PORT = 5000;
 
-app.listen(PORT);  //server needs to constantly be moving (request, receive, request, receive, etc.)
+app.get('/', function (req, res) { //req is request, res is response
+    res.send('Hello World');
+})
+
+app.get('/hey', function (req, res) { //req is request, res is response
+    res.send('howdy');
+})
+
+app.listen(PORT, function () {
+    console.log(`listening on port:  ${PORT}F`)
+});
+//server needs to constantly be moving (request, receive, request, receive, etc.)
+
